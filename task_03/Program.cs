7b3024d2333fb0cf89f17d4ef7991a85387b5c46
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-12 11:31:15 +0300

    Написать программу вычисления произведения чисел от 1 до N

---
+++ task_03/Program.cs
@@ -0,0 +1,25 @@
+﻿// Task_4 Написать программу вычисления произведения чисел от 1 до N
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int c = new Random().Next(1, 10);
+int d = new Random().Next(1, 10);
+int e = new Random().Next(1, 10);
+int f = new Random().Next(1, 10);
+int g = new Random().Next(1, 10);
+int i = 0;
+int prod = 1;
+int[] numbers = {a, b, c, d, e, f, g};
+
+while (i < numbers.Length)
+{
+    Console.Write(numbers[i] + " ");
+    i++;
+}
+i = 0;
+Console.WriteLine("");
+while (i < numbers.Length)
+{
+    prod = prod * numbers[i];
+    i++;
+}
+Console.WriteLine(prod);
