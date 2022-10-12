151e7f5d9f0daeb1c80f45515ce0d8f3910f8f4c
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-12 11:31:44 +0300

    Показать кубы чисел, заканчивающихся на четную цифру

---
+++ task_04/Program.cs
@@ -0,0 +1,29 @@
+﻿// Task_04 Показать кубы чисел, заканчивающихся на четную цифру
+int a = new Random().Next(1, 10);
+int b = new Random().Next(1, 10);
+int c = new Random().Next(1, 10);
+int d = new Random().Next(1, 10);
+int e = new Random().Next(1, 10);
+int f = new Random().Next(1, 10);
+int g = new Random().Next(1, 10);
+int i = 0;
+int prod = 1;
+int[] numbers = {a, b, c, d, e, f, g};
+
+while (i < numbers.Length)
+{
+    Console.Write(numbers[i] + " ");
+    i++;
+}
+
+i = 0;
+Console.WriteLine("");
+
+while (i < numbers.Length)
+{
+    if(numbers[i]%2 == 0)
+    {
+        Console.WriteLine(Math.Pow(numbers[i], 3));
+    }
+    i++;
+}
