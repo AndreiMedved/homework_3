c4c5fabbb6562b379d68b7b150dedbfe2d57982a
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-12 11:30:04 +0300

    Найти кубы чисел от 1 до N

---
+++ task_01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Task_01 Найти кубы чисел от 1 до N
+
+int a = new Random().Next(-10, 10);
+int b = new Random().Next(-10, 10);
+int c = new Random().Next(-10, 10);
+int d = new Random().Next(-10, 10);
+int e = new Random().Next(-10, 10);
+int f = new Random().Next(-10, 10);
+int g = new Random().Next(-10, 10);
+int i = 0;
+int[] numbers = {a, b, c, d, e, f, g};
+
+while (i < numbers.Length)
+{
+    Console.Write(numbers[i] + " ");
+    i++;
+}
+
+i = 0;
+Console.WriteLine("");
+
+while (i < numbers.Length)
+{
+    Console.WriteLine(Math.Pow(numbers[i], 3));
+    i++;
+}
