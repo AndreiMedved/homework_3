53be48357ca9550588021c4dc9c910fc5868d0a7
Медведев Андрей Александрович <MedvedevAA@mt.local>
2022-10-12 11:30:41 +0300

    Подсчитать сумму цифр в числе

---
+++ task_02/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task_02 Подсчитать сумму цифр в числе
+
+int a = new Random().Next(100, 1000);
+String number = Convert.ToString(a);
+char n1 = number[0];
+char n2 = number[1];
+char n3 = number[2];
+int num0 = n1 - '0';
+int num1 = n2 - '0';
+int num2 = n3 - '0';
+
+Console.WriteLine(num0 + " " + num1 + " " + num2);
+Console.WriteLine(num0 + num1 + num2);
+
+
+
+
